d8d8cf786f3f0575e42720dc87e360f880725c58
Ajeet <ajeetrocks100@gmail.com>
2018-10-09 01:07:24 +0530

    Update tensorflow.py

--- tensorflow.py
+++ tensorflow.py
@@ -2,4 +2,5 @@ import tensorflow as tf
 hello=tf.constant("Hello World")
 sess=tf.session()
 print(sess.run(hello))
+print('done')
 ##Don't forget to load tensorflow files first. This program is only to check whether Tensorflow is installed in your system or not.
