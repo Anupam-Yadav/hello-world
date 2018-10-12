abe7e9d5c16f143dd97fa29b6789e17e1bf36b66
Ajeet <ajeetrocks100@gmail.com>
2018-10-13 02:45:43 +0530

    Update tensorflow.py (#15)

--- tensorflow.py
+++ tensorflow.py
@@ -4,4 +4,5 @@ print('start')
 sess=tf.session()
 print(sess.run(hello))
 print('done')
-##Don't forget to load tensorflow files first. This program is only to check whether Tensorflow is installed in your system or not.
+##Don't forget to load tensorflow files first.
+##This program is only to check whether Tensorflow is installed in your system or not.
