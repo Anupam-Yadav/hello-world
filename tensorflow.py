3611fa31e640f92a8b8edc8238eaa664a9f149d7
Ajeet <ajeetrocks100@gmail.com>
2020-10-15 23:44:48 +0530

    Update tensorflow.py

--- tensorflow.py
+++ tensorflow.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-# Install TensorFlow
+# Install TensorFlow method
 try:
   # %tensorflow_version only exists in Colab.
   %tensorflow_version 2.x
